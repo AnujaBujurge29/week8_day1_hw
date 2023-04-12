eae17413b2c3a05021a4026b9daf72044c995dda
AnujaBujurge29 <anuja.bujurge29@gmail.com>
2023-04-12 09:34:07 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Counter from "./Components/Counter";
+import "./App.css";
 
 function App() {
+  // const [counter, setCounter] = useState(0);
+  // const addOne = () => {
+  //   setCounter(counter + 1);
+  // };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+      <div className="block b1">
+        <p className="aside-bar">
+        <p>DashBoard</p>
+        <p>Widget</p>
+        <p>Reviews</p>
+        <p>Customers</p>
+        <p>Online Analysis</p>
+        <p>Settings</p>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+
+      <div className="block b2">Reviews<br/><br/>1,281</div>
+      <div className="block b3">Average Rating<br/><br/>4.6</div>
+      <div className="block b4">Sentiment Analysis<br/><br/>960<br/>122<br></br>321</div>
+      <div className="block b5">Website Subscribers<br/><br/>
+      <Counter></Counter>
+      </div>
+   
+      {/* <button className="Button" onClick={addOne}><h3>Subscribe Here</h3></button> */}
+      </div>
+    
   );
 }
 
